cc1261b972d8328f8d2cbadf7ce55ae066bb59f6
dylan <dylancoalwell@gmail.com>
2016-05-29 19:23:39 -0500

    release 2 complete

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,7 +2,7 @@ puts "What is the hamster's name?"
 name = gets.chomp
 
 puts "How noisy is the hamster? (1 to 10)"
-volume = gets.chomp.tp_i
+volume = gets.chomp.to_i
 
 puts "What color is the hamster's fur?"
 fur_color = gets.chomp
@@ -10,10 +10,20 @@ fur_color = gets.chomp
 puts "Is the hamster a good candidate for adoption? (y/n)"
 is_adoptable = gets.chomp
 
+if is_adoptable == "y"
+	is_adoptable = true
+elsif is_adoptable == "n"
+	is_adoptable = false
+else
+	is_adoptable = nil
+end
+
 puts "What is the estimated age of the hamster? (in years)"
 age = gets.chomp
 
 if age == ""
 	age = nil
+else
+	age = age.to_f
 end
 
