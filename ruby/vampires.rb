b45c5b3fd1d0300059c53e8490b6e9f668ff5856
Dylan Coalwell <dylancoalwell@gmail.com>
2016-05-30 14:11:06 -0500

    release 1 complete

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+g_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance? (y/n)"
+health_insurance = gets.chomp
