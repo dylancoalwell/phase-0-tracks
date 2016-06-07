6f66e0505489e43937f19fd2fe3300db9825d6af
Dylan Coalwell <dylancoalwell@gmail.com>
2016-06-07 17:50:52 -0500

    Finished Release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,11 @@
 class Santa
 
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		puts "Initializing Santa instance..."
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -8,12 +14,17 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
-	def initialize
-		puts "Initializing Santa instance..."
-	end
+	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	age = 0
 
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("chocolate chunk cookie")
+santas = []
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+genders.length.times do |i|
+  santas << Santa.new(genders[i], ethnicities[i])
+end
+
+santas[1].speak
+santas[4].eat_milk_and_cookies("sugar cookie")
