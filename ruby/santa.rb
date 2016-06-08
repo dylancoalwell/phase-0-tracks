17974dda6096a56441c4afe76a6a857371e45770
Dylan Coalwell <dylancoalwell@gmail.com>
2016-06-07 21:22:53 -0500

    Finished Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,6 +3,8 @@ class Santa
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 		puts "Initializing Santa instance..."
 	end
 
@@ -14,11 +16,34 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
-	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	age = 0
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		if @reindeer_ranking.include?(reindeer) == true
+			@reindeer_ranking.delete_if { |name| name == reindeer }
+			@reindeer_ranking << reindeer
+		else
+			puts "There isn't a reindeer with the name #{reindeer}."
+		end
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
 
 end
 
+=begin
 santas = []
 genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
@@ -28,3 +53,14 @@ end
 
 santas[1].speak
 santas[4].eat_milk_and_cookies("sugar cookie")
+=end
+
+santa = Santa.new("male", "white")
+p santa
+santa.get_mad_at("Dasher")
+santa.get_mad_at("Billy")
+santa.celebrate_birthday
+santa.gender = "polyamorous"
+p santa.age
+p santa.ethnicity
+p santa
