f0f4b55bab214f38d7ff6f15018e2c283714ae86
dylan <dylancoalwell@gmail.com>
2016-06-08 20:39:54 -0500

    Finished Release 4

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,14 +1,14 @@
-=begin
+
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps: 
-  # take the items that are being inputted.
+  # take the items that are being input.
   # set default quantity
   # print the list to the console [can you use one of your other methods here?]
 # output: hash
 
 # Method to add an item to a list
-# input: item name and optional quantity
+# input: list name, item name and optional quantity
 # steps: 
   # input the name
   # input the quantity
@@ -16,7 +16,7 @@
 # output: string
 
 # Method to remove an item from the list
-# input: item that you would like to remove
+# input: list name and item that you would like to remove
 # steps: 
   # input the name
   # remove that item from the list.
@@ -24,7 +24,7 @@
 # output: string
 
 # Method to update the quantity of an item
-# input: item name and quantity
+# input: list name, item name and quantity
 # steps:
   # input name
   # input quantity
@@ -33,11 +33,48 @@
 # output: string
 
 # Method to print a list and make it look pretty
-# input: no input
+# input: list name as a string
 # steps: 
-  # print the name of the listt
+  # print the name of the list
   # print the name of the items
   # print the mame of the quantities 
 # output: string
-=end
 
+
+def create_grocery_list(items)
+	grocery_list = {}
+	grocery_array = items.split(" ")
+	grocery_array.each do |item|
+		grocery_list[item] = 1
+	end
+	grocery_list
+end
+
+
+def add_item(list_name, item, quantity)
+	list_name[item] = quantity
+end
+
+def remove_item(list_name, item)
+	list_name.delete(item)
+end
+
+def update_quantity(list_name, item, new_quantity)
+	list_name[item] = new_quantity
+end
+
+def print_list(list_name, list_name_as_string)
+	puts "#{list_name_as_string}:"
+	list_name.each do |item, quantity|
+		puts "#{item}: #{quantity}."
+	end
+end
+
+p grocery_list = create_grocery_list("banana carrot")
+add_item(grocery_list, "orange", 4)
+p grocery_list
+remove_item(grocery_list, "banana")
+p grocery_list
+update_quantity(grocery_list, "carrot", 6)
+p grocery_list
+print_list(grocery_list, "My Grocery List")
