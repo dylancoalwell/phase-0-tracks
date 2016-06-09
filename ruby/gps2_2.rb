d6ae141ac4dcc1917c1f8643fde807bf3bb807aa
dylan <dylancoalwell@gmail.com>
2016-06-08 19:21:21 -0500

    Finished Release 1

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,43 @@
+=begin
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # take the items that are being inputted.
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: hash
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: 
+  # input the name
+  # input the quantity
+  # print the item and quantity.
+# output: string
+
+# Method to remove an item from the list
+# input: item that you would like to remove
+# steps: 
+  # input the name
+  # remove that item from the list.
+  # print to the console the item name and that it was removed.
+# output: string
+
+# Method to update the quantity of an item
+# input: item name and quantity
+# steps:
+  # input name
+  # input quantity
+  # update the list
+  # print a message to the console saying what item was updated and how it was updated.
+# output: string
+
+# Method to print a list and make it look pretty
+# input: no input
+# steps: 
+  # print the name of the listt
+  # print the name of the items
+  # print the mame of the quantities 
+# output: string
+=end
+
