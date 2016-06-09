9d8f4a9405bdccc53f754df5f997a5460f8b148d
dylan <dylancoalwell@gmail.com>
2016-06-08 21:09:14 -0500

    Finished release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(x)
+  	x.times { puts "Woof!" }
+  end
+
+  def roll_over
+  	puts "*Rolls over*"
+  end
+
+  def dog_years(human_years)
+  	human_years * 7
+  end
+
+  def play_dead
+  	puts "*falls on floor and sticks tongue out of mouth*"
+  end
+
 end
 
+max = Puppy.new
+max.fetch("ball")
+max.speak(6)
+max.roll_over
+p max.dog_years(23)
+max.play_dead
