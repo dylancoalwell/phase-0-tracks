86932763e2c4ee2477c73a8d44ead71299d1d5b5
Dylan Coalwell <dylancoalwell@gmail.com>
2016-06-09 20:43:53 -0500

    Finished Release 0

---
+++ ruby/solo.rb
@@ -0,0 +1,19 @@
+=begin
+	
+Design a Movie Class
+
+Input a Name/Title (String)
+
+Attributes###############
+#Genre: varies (string)
+#Length in minutes: varies (integer)
+#Location: varies (string)
+#Lead Actors/Actresses: varies (integer)
+
+Methods###################
+#A method that plays the movie. Doesn't take any input.
+#A method that fast-forwards the movie. Takes a float that determines the speed.
+#A method that pauses the movie. Doesn't take any input.
+#A method that adds subtitles. Takes a string for the desired language.
+	
+=end
