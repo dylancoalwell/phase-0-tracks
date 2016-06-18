85c10f01ca297887bfae980c60e6a6690f9d4621
dylan <dylancoalwell@gmail.com>
2016-06-18 12:30:18 -0500

    finished release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,11 @@ var name = ["greg", "samantha", "billy", "jessica"];
 
 colors.push("green");
 name.push("jeff");
+
+var horses = {}
+
+for (var i = 0; i < name.length; i++){
+  horses[name[i]] = colors[i];
+}
+
+console.log(horses);
