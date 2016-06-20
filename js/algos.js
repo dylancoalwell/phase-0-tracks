774a6e10fed68862b98fc679d9141b90633d9fcb
Dylan Coalwell <dylancoalwell@gmail.com>
2016-06-20 02:02:52 -0500

    still working on release 1.

--- js/algos.js
+++ js/algos.js
@@ -15,7 +15,29 @@ function longestWord(array)	{
 	console.log(longest);
 }
 
+/* Create function that takes two hashes
+Compare the keys and values of the hashes.
+If the hashes share a key pair or a value pair return true.
+If they don't share either return false.
+*/
+
+function compareObjects(object1, object2) {
+	for(var i = 0; i < array.length; i++){
+	    if(object1[i] == object2[i] && object1[i][i] == object2[i][i]) {
+	    	return true;
+	    }
+	    else {
+	    	return false;
+	    }
+	}
+}
+
+
 var longArray = ["short", "not long", "This is totally the longest", "tiny"];
 var longArray2 = ["No way this is longest", "This is way longer I bet."];
 longestWord(longArray);
 longestWord(longArray2);
+
+var object1 = {name: "Steven", age: 54};
+var object2 = {name: "Tamir", age: 54};
+compareObjects(object1, object2);
