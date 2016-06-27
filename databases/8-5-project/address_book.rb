db2d2d1e1a9446e9a6d5ef8000b93f570d183eca
Dylan Coalwell <dylancoalwell@gmail.com>
2016-06-26 21:36:35 -0500

    Pseudocoded.

---
+++ databases/8-5-project/address_book.rb
@@ -0,0 +1,11 @@
+# Making a fake address book for people who want to create the illusion of having a lot of friends.
+
+###################### OBJECTIVES ######################################################################
+# The user should be able to provied input. They should be able provide how many fake contacts the want. 
+# They should also be able to C.R.U.D. the database.
+# Define a method that creates an address book database that includes fake name, phone number, email address, street address, city and country for fake contacts.
+# Include FAKER gem for creating fake contact info.
+# Create an interface with which the user can C.R.U.D. the database.
+# User should be able to print out their address book in a readable format.
+########################################################################################################
+
