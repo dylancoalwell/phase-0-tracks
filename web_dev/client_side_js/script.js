75944c36e35d8aa7d12ed249543d9ada529728a7
Dylan Coalwell <dylancoalwell@gmail.com>
2016-07-10 00:07:23 -0500

    added event listener to js page.

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,9 @@
 console.log("This script is running!");
+
+function greenOutline() {
+	var photo = document.getElementById("logo");
+	photo.style.border = "4px solid green";
+}
+
+var photo = document.getElementById("logo");
+photo.addEventListener("click", greenOutline);
